6cd272a34460eac51f9f9706b6752298e4804736
Brine <104254430+brinereef1@users.noreply.github.com>
2023-08-23 20:05:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,30 @@
 const audioContainers = document.querySelectorAll('.audio-container');
 const audioPlayers = document.querySelectorAll('.audioPlayer');
 
-function playAudio(index) {
-  const playIcon = audioContainers[index].querySelector('.playIcon');
-  const pauseIcon = audioContainers[index].querySelector('.pauseIcon');
-  const audioPlayer = audioPlayers[index];
-
-  audioPlayer.play();
-  playIcon.style.display = 'none';
-  pauseIcon.style.display = 'inline';
-
-  audioPlayer.addEventListener('ended', function () {
-    playIcon.style.display = 'inline';
-    pauseIcon.style.display = 'none';
-    if (index < audioContainers.length - 1) {
-      playAudio(index + 1);
-    }
-  });
-}
-
 audioContainers.forEach((container, index) => {
   const playIcon = container.querySelector('.playIcon');
   const pauseIcon = container.querySelector('.pauseIcon');
   const audioPlayer = audioPlayers[index];
 
   playIcon.addEventListener('click', function () {
-    if (audioPlayer.paused) {
-      audioPlayers.forEach((player, playerIndex) => {
-        if (playerIndex !== index && !player.paused) {
-          player.pause();
-          audioContainers[playerIndex].querySelector('.playIcon').style.display = 'inline';
-          audioContainers[playerIndex].querySelector('.pauseIcon').style.display = 'none';
-        }
-      });
 
-      playAudio(index);
+    audioPlayers.forEach((player, playerIndex) => {
+      if (playerIndex !== index && !player.paused) {
+        player.pause();
+        audioContainers[playerIndex].querySelector('.playIcon').style.display = 'inline';
+        audioContainers[playerIndex].querySelector('.pauseIcon').style.display = 'none';
+      }
+    });
+
+    if (audioPlayer.paused) {
+      audioPlayer.currentTime = 0;
+      audioPlayer.play();
+      playIcon.style.display = 'none';
+      pauseIcon.style.display = 'inline';
+    } else {
+      audioPlayer.pause();
+      playIcon.style.display = 'inline';
+      pauseIcon.style.display = 'none';
     }
   });
 
@@ -43,6 +33,19 @@ audioContainers.forEach((container, index) => {
       audioPlayer.pause();
       playIcon.style.display = 'inline';
       pauseIcon.style.display = 'none';
+    }
+  });
+
+  audioPlayer.addEventListener('ended', function () {
+    playIcon.style.display = 'inline';
+    pauseIcon.style.display = 'none';
+
+    if (index < audioContainers.length - 1) {
+      const nextAudioPlayer = audioPlayers[index + 1];
+      nextAudioPlayer.currentTime = 0;
+      nextAudioPlayer.play();
+      audioContainers[index + 1].querySelector('.playIcon').style.display = 'none';
+      audioContainers[index + 1].querySelector('.pauseIcon').style.display = 'inline';
     }
   });
 });
